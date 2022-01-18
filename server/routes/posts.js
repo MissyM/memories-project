2e32dd1a367ac6b10985eb9010b86f1661da5b1a
MissyM <mcabrerapatino@gmail.com>
2022-01-18 10:45:51 -0500

    backend setup and frontend setup

---
+++ server/routes/posts.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+import { getPosts, createPosts } from '../controllers/posts.js'
+
+const router = express.Router();
+
+//localhost:5000/posts
+
+router.get('/', getPosts)
+router.post('/', createPosts)
+
+export default router;
